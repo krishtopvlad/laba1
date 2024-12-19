5397349b145f9b794a4f1df3af49bca81ec4ead1
Владислав К <vladkryshtop@gmail.com>
2024-12-19 14:47:09 +0300

    update1

--- src/bsu/rfe/java/group6/lab1/Krishtop/var10/Consumable.java
+++
@@ -1,5 +0,0 @@
-package bsu.rfe.java.group6.lab1.Krishtop.var10;
-
-public interface Consumable {
-    public abstract void consume();
-}
--- src/bsu/rfe/java/group6/lab1/Krishtop/var10/Nutritious.java
+++
@@ -1,5 +0,0 @@
-package bsu.rfe.java.group6.lab1.Krishtop.var10;
-
-public interface Nutritious {
-    double calculateCalories();
-}
